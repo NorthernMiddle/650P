8779d31e23998e3c12057ce52f4e12ce3dcffbc1
nmsMentor <43293575+nmsMentor@users.noreply.github.com>
2018-09-16 00:08:51 -0400

    Update shooter.cpp

--- sources/shooter.cpp
+++ sources/shooter.cpp
@@ -1,6 +1,8 @@
 /**
- * shooter.cpp - some program to control shooter system
+ * shooter.cpp - some program to control shooter mechanism
  *
+ * written for Northern Middle School Robotics
+ * 2018-19 VRC "Turning Point"
  *
  *
  * @TODO:
