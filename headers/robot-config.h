130f82f0b835bf7b9199046a6ba781ae3f58376c
nmsMentor <43293575+nmsMentor@users.noreply.github.com>
2018-09-15 23:10:06 -0400

    Create robot-config.h

---
+++ headers/robot-config.h
@@ -0,0 +1,18 @@
+/**
+ * robot-config.h - header file for VRC V5
+ * written for Northern Middle School Robotics Team 650P
+ * VRC 2018-19 "Turning Point"
+ */
+ 
+ using namespace vex;
+ vex::brain Brain;
+ 
+//drive motors
+vex::motor leftMotor (vex::PORT1, vex::gearSetting::ratios18_1, false);
+vex::motor rightMotor (vex::PORT10,  vex::gearSetting::ratio18_1, true);
+
+//claw motor
+vex::motor clawMotor (vex::PORT3, vex::gearSetting::ratio18_1,true);
+
+//arm motor
+vex::motor armMotor (vex::PORT8, vex::gearSetting::ratio18_1,true);
