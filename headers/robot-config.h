c4650aeca0004552d1a335b8b31db7bbd03a824b
nmsMentor <43293575+nmsMentor@users.noreply.github.com>
2018-09-15 23:38:59 -0400

    Update robot-config.h

--- headers/robot-config.h
+++ headers/robot-config.h
@@ -2,6 +2,11 @@
  * robot-config.h - header file for VRC V5
  * written for Northern Middle School Robotics Team 650P
  * VRC 2018-19 "Turning Point"
+ *
+ * @TODO: add setup code for collector mechanism
+ * @TODO: add setup code for shooter mechansim
+ * @TODO: add setup code for V5 vision sensor
+ * @TODO: 
  */
  
  using namespace vex;
@@ -17,10 +22,10 @@ vex::motor clawMotor (vex::PORT3, vex::gearSetting::ratio18_1,true);
 //arm motor
 vex::motor armMotor (vex::PORT8, vex::gearSetting::ratio18_1,true);
 
-//collector
+//collector mechanism
 ????
  
-//flywheel (shooter mechanism)
+//shooter mechanism
 ????
  
 //V5 vision sensor
